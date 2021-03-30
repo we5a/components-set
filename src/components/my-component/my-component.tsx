1507b4354bb7057ac64160ccfad1fbc6c29fb116
Yaroslav_S <leafset@gmail.com>
2021-03-30 15:30:51 +0300

    init

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,38 @@
+import { Component, Prop, h, Method } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  @Method()
+  sayHello() {
+    console.log('Say some hello from the end!');
+    return Promise.resolve(45);
+  }
+
+  render() {
+    return <div style={{textAlign: 'center'}}>Hello#, World! I'm from Stencil with love {this.getText()}</div>;
+  }
+}
---
+++ src/components/my-component/readme.md
@@ -0,0 +1,32 @@
+# my-component
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description     | Type     | Default     |
+| -------- | --------- | --------------- | -------- | ----------- |
+| `first`  | `first`   | The first name  | `string` | `undefined` |
+| `last`   | `last`    | The last name   | `string` | `undefined` |
+| `middle` | `middle`  | The middle name | `string` | `undefined` |
+
+
+## Methods
+
+### `sayHello() => Promise<number>`
+
+
+
+#### Returns
+
+Type: `Promise<number>`
+
+
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
